42f3b30c75a9c3915803ed62eea8fb8e04b492eb
LimJunxue <junxue1@hotmail.com>
2020-08-15 22:47:36 +0800

    Update AnnouncementPage.js

--- src/components/AnnouncementPage.js
+++ src/components/AnnouncementPage.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const AnnoucementPage = () => {
-	return (
-		<div>
-			<h1>Announcement page</h1>
-		</div>
-	);
+  return (
+    <div>
+      <h1>Junxue was here</h1>
+    </div>
+  );
 };
 
 export default AnnoucementPage;
