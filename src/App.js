9305162e64b47867e47b943e5a37c7d6ec685737
hansebastian <sebastian_hans@hotmail.com>
2020-08-16 16:43:13 +0800

    Proper login form, unlinked to backend

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect, Link } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import TopNavbar from './components/TopNavbar';
 import AnnouncementPage from './components/AnnouncementPage';
 import LoginPage from './components/LoginPage';
+import AdminView from './components/admin/AdminView';
+
+// Keeps track of whether user is authenticated as staff / admin.
+var authObj = {
+	isAuthenticated: true,
+	authenticate(newLocation) {
+		authObj.isAuthenticated = true;
+		setTimeout(newLocation, 1000); // fake async, swap with actual authentication
+	},
+	signout(newLocation) {
+		authObj.isAuthenticated = false;
+		setTimeout(newLocation, 1000);
+	},
+};
 
 export default function App() {
 	return (
 		<Router>
 			<TopNavbar />
 			<Switch>
+				<PrivateRoute path='/administrator'>
+					<AdminView />
+				</PrivateRoute>
 				<Route path='/announcements/'>
 					<AnnouncementPage />
 				</Route>
@@ -23,3 +40,27 @@ export default function App() {
 		</Router>
 	);
 }
+
+/**
+ * Only grant access to these pages if user is logged in as administrator or staff
+ * Otherwise, redirect to login.
+ */
+const PrivateRoute = ({ children, ...rest }) => {
+	return (
+		<Route
+			{...rest}
+			render={({ location }) =>
+				authObj.isAuthenticated ? (
+					children
+				) : (
+					<Redirect
+						to={{
+							pathname: '/login',
+							state: { from: location },
+						}}
+					/>
+				)
+			}
+		/>
+	);
+};
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,57 +1,140 @@
 import React from 'react';
-import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { Formik, Form, useField } from 'formik';
+import styled from '@emotion/styled';
+import './FormStyles.css';
+import * as Yup from 'yup';
+import { useHistory, useLocation } from 'react-router-dom';
+
+const MyTextInput = ({ label, ...props }) => {
+	const [field, meta] = useField(props);
+	return (
+		<>
+			<label htmlFor={props.id || props.name}>{label}</label>
+			<input className='text-input' {...field} {...props} />
+			{meta.touched && meta.error ? <div className='error'>{meta.error}</div> : null}
+		</>
+	);
+};
+
+// Styled components ....
+const StyledSelect = styled.select`
+	color: var(--blue);
+`;
+
+const StyledErrorMessage = styled.div`
+	font-size: 12px;
+	color: var(--red-600);
+	width: 400px;
+	margin-top: 0.25rem;
+	&:before {
+		content: '❌ ';
+		font-size: 10px;
+	}
+	@media (prefers-color-scheme: dark) {
+		color: var(--red-300);
+	}
+`;
+
+const StyledLabel = styled.label`
+	margin-top: 1rem;
+`;
+
+const MySelect = ({ label, ...props }) => {
+	const [field, meta] = useField(props);
+	return (
+		<>
+			<StyledLabel htmlFor={props.id || props.name}>{label}</StyledLabel>
+			<StyledSelect {...field} {...props} />
+			{meta.touched && meta.error ? (
+				<StyledErrorMessage>{meta.error}</StyledErrorMessage>
+			) : null}
+		</>
+	);
+};
 
 const LoginPage = () => {
+	// Query database to verify login information
+	const onSubmit = (formParameters) => {
+		if (true) {
+			// Access granted, redirect to admin access website
+			history.push('/administrator');
+		} else {
+			// Access denied
+		}
+		alert(formParameters);
+	};
+
+	let history = useHistory();
+	let location = useLocation();
+
+	let { from } = location.state || { from: { pathname: '/' } };
+	let login = () => {};
+
 	return (
-		<div>
-			<h1>Login form</h1>
-			<Form style={{ marginLeft: '5%', marginRight: '5%' }}>
-				<FormGroup>
-					<Label for='exampleEmail'>Email</Label>
-					<Input
-						type='email'
-						name='email'
-						id='exampleEmail'
-						placeholder='farrer.park@gmail.com'
-					/>
-				</FormGroup>
-				<FormGroup>
-					<Label for='examplePassword'>Password</Label>
-					<Input
-						type='password'
-						name='password'
-						id='examplePassword'
-						placeholder='password'
-					/>
-				</FormGroup>
-				<FormGroup>
-					<Label for='exampleSelect'>Select</Label>
-					<Input type='select' name='select' id='exampleSelect'>
-						<option>Administrator</option>
-						<option>Visitor</option>
-						<option>Staff</option>
-					</Input>
-				</FormGroup>
-				<FormGroup tag='fieldset'>
-					<legend>Access Privileges (choose this or drop down selection)</legend>
-					<FormGroup check>
-						<Label check>
-							<Input type='radio' name='radio1' />I am an administrator
-						</Label>
-					</FormGroup>
-					<FormGroup check>
-						<Label check>
-							<Input type='radio' name='radio1' />I am a visitor
-						</Label>
-					</FormGroup>
-					<FormGroup check disabled>
-						<Label check>
-							<Input type='radio' name='radio1' />I am a staff
-						</Label>
-					</FormGroup>
-				</FormGroup>
-				<Button>Submit</Button>
-			</Form>
+		<div
+			style={{
+				display: 'flex',
+				flexDirection: 'column',
+				alignItems: 'center',
+			}}
+		>
+			<div>
+				<h2>Staff Login</h2>
+				<>
+					<Formik
+						initialValues={{
+							staffID: '',
+							password: '',
+							accessType: '', // added for our select
+						}}
+						validationSchema={Yup.object({
+							staffID: Yup.string()
+								.max(15, 'Must be 15 characters or less')
+								.required('Required'),
+							password: Yup.string()
+								.max(15, 'Must be 15 characters or less')
+								.required('Required'),
+							accessType: Yup.string()
+								.oneOf(['Administrator', 'Staff'], 'Invalid Job Type')
+								.required('Required'),
+						})}
+						onSubmit={(values, { setSubmitting }) => {
+							setTimeout(() => {
+								onSubmit(JSON.stringify(values, null, 2));
+								setSubmitting(false);
+							}, 400);
+						}}
+					>
+						<Form
+							style={{
+								alignItems: 'flex-start',
+								flexDirection: 'column',
+								display: 'flex',
+							}}
+						>
+							<MyTextInput
+								label='Staff ID'
+								name='staffID'
+								type='text'
+								placeholder='A0112298T'
+							/>
+							<MyTextInput
+								label='Password'
+								name='password'
+								type='password'
+								placeholder='Password'
+							/>
+							<MySelect label='Access' name='accessType'>
+								<option value=''>Access Type</option>
+								<option value='Administrator'>Administrator</option>
+								<option value='Staff'>Staff</option>
+							</MySelect>
+
+							<button type='submit'>Submit</button>
+						</Form>
+					</Formik>
+				</>
+			</div>
 		</div>
 	);
 };
