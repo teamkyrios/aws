e570fcc55a72ce0f7293025d24e00e2223ff76f7
RuiFeng <changruifeng98@gmail.com>
2020-08-16 23:19:18 +0800

    switch to formik

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,90 +1,107 @@
 import React from "react";
-import { Button, Form, FormGroup, Label, Input } from "reactstrap";
+import { Formik, Form, useField } from "formik";
+import styled from "@emotion/styled";
+import "./FormStyles.css";
+import * as Yup from "yup";
 
 const LandingPage = () => {
+  const onSubmit = (formParameters) => {
+    // Check form parameters and do necessary
+    console.log("submitting");
+  };
   return (
     <div
       style={{
-        flex: 1,
-        justifyContent: "center",
+        display: "flex",
+        flexDirection: "column",
         alignItems: "center",
       }}
     >
-      <h2 style={{ marginLeft: 450 }}>Visitor Registration</h2>
-      <Form>
-        <FormGroup style={{}}>
-          <Label style={styles.label} for="exampleName">
-            Full Name
-          </Label>
-          <Input
-            style={styles.input}
-            type="text"
-            name="name"
-            id="exampleName"
-            placeholder="John Smith"
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label style={styles.label} for="exampleNric">
-            Your NRIC/Passport Number
-          </Label>
-          <Input
-            style={styles.input}
-            type="text"
-            name="nric"
-            id="exampleNric"
-            placeholder="S1234567Z"
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label style={styles.label} for="exampleContact">
-            Contact Number
-          </Label>
-          <Input
-            style={styles.input}
-            type="text"
-            name="contact"
-            id="exampleContact"
-            placeholder="91234567"
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label style={styles.label} for="examplePatient">
-            Patient Name
-          </Label>
-          <Input
-            style={styles.input}
-            type="text"
-            name="patientName"
-            id="examplePatient"
-            placeholder="Mary Jane"
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label style={styles.label} for="examplePatientNric">
-            Patient NRIC/Passport Number
-          </Label>
-          <Input
-            style={styles.input}
-            type="text"
-            name="nric"
-            id="examplePatientNric"
-            placeholder="S1234567Z"
-          />
-        </FormGroup>
-        <Button style={{ marginLeft: 720 }}>Submit</Button>
-      </Form>
+      <div>
+        <h2>Visitor Registration</h2>
+        <>
+          <Formik
+            initialValues={{
+              name: "",
+              nric: "",
+              contact: "",
+              patientName: "",
+              patientNric: "",
+            }}
+            validationSchema={Yup.object({
+              name: Yup.string().required("Required"),
+              nric: Yup.string().required("Required"),
+              contact: Yup.string().required("Required"),
+              patientName: Yup.string().required("Required"),
+              patientNric: Yup.string().required("Required"),
+            })}
+            onSubmit={(values, { setSubmitting }) => {
+              setTimeout(() => {
+                onSubmit(JSON.stringify(values, null, 2));
+                setSubmitting(false);
+              }, 400);
+            }}
+          >
+            <Form
+              style={{
+                alignItems: "flex-start",
+                flexDirection: "column",
+                display: "flex",
+              }}
+            >
+              <MyTextInput
+                label="Your Name"
+                name="name"
+                type="text"
+                placeholder="John Smith"
+              />
+              <MyTextInput
+                label="Your NRIC/Passport Number"
+                name="nric"
+                type="text"
+                placeholder="S1234567A"
+              />
+              <MyTextInput
+                label="Your Contact Number"
+                name="contact"
+                type="number"
+                placeholder="91234567"
+              />
+              <MyTextInput
+                label="Patient Name"
+                name="patientName"
+                type="text"
+                placeholder="Mary Jane"
+              />
+              <MyTextInput
+                label="Patient NRIC/Passport Number"
+                name="patientNric"
+                type="text"
+                placeholder="S9876543Z"
+              />
+
+              <button style={{ marginBottom: 20 }} type="submit">
+                Submit
+              </button>
+            </Form>
+          </Formik>
+        </>
+      </div>
     </div>
   );
 };
 
-let styles = {
-  input: {
-    width: 400,
-    marginLeft: 400,
-  },
-  label: {
-    marginLeft: 400,
-  },
+const MyTextInput = ({ label, ...props }) => {
+  const [field, meta] = useField(props);
+  return (
+    <>
+      <label htmlFor={props.id || props.name}>{label}</label>
+      <input className="text-input" {...field} {...props} />
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
+    </>
+  );
 };
+
 export default LandingPage;
