d4462d57ead7318ff30124ea7e50d081028bafca
RuiFengg <ruifeng.chang98@hotmail.com>
2020-08-24 22:56:53 +0800

    Move chatbot to landing page

--- src/App.js
+++ src/App.js
@@ -1,127 +1,74 @@
-// import React from "react";
-// import { connect } from "react-redux";
-// import {
-//     BrowserRouter as Router,
-//     Switch,
-//     Route,
-//     Redirect,
-//     Link,
-// } from "react-router-dom";
-// import LandingPage from "./components/LandingPage";
-// import TopNavbar from "./components/TopNavbar";
-// import AnnouncementPage from "./components/AnnouncementPage";
-// import LoginPage from "./components/LoginPage";
-// import AdminView from "./components/admin/AdminView";
-// import Amplify, { Interactions } from "aws-amplify";
-// import { ChatBot, AmplifyTheme } from "aws-amplify-react";
-// import awsconfig from "./aws-exports";
+import React from "react";
+import { connect } from "react-redux";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Redirect,
+    Link,
+} from "react-router-dom";
+import LandingPage from "./components/LandingPage";
+import TopNavbar from "./components/TopNavbar";
+import AnnouncementPage from "./components/AnnouncementPage";
+import LoginPage from "./components/LoginPage";
+import AdminView from "./components/admin/AdminView";
+import Amplify, { Interactions } from "aws-amplify";
+import { ChatBot, AmplifyTheme } from "aws-amplify-react";
+import awsconfig from "./aws-exports";
 
-// // Amplify.configure(awsconfig);
-// // const myTheme = {
-// //     ...AmplifyTheme,
-// //     sectionHeader: {
-// //         ...AmplifyTheme.sectionHeader,
-// //         backgroundColor: "#ff6600",
-// //     },
-// // };
+Amplify.configure(awsconfig);
 
-// function App({ isAuthenticated }) {
-//     /**
-//      * Only grant access to these pages if user is logged in as administrator or staff
-//      * Otherwise, redirect to login.
-//      */
-//     const PrivateRoute = ({ children, ...rest }) => {
-//         return (
-//             <Route
-//                 {...rest}
-//                 render={({ location }) =>
-//                     isAuthenticated ? (
-//                         children
-//                     ) : (
-//                         <Redirect
-//                             to={{
-//                                 pathname: "/login",
-//                                 state: { from: location },
-//                             }}
-//                         />
-//                     )
-//                 }
-//             />
-//         );
-//     };
-
-//     return (
-//         <Router>
-//             <TopNavbar />
-
-//             <Switch>
-//                 <Route exact path="/">
-//                     <LandingPage />
-//                 </Route>
-//                 <Route path="/login">
-//                     <LoginPage />
-//                 </Route>
-//                 <Route path="/announcements">
-//                     <AnnouncementPage />
-//                 </Route>
-//                 <PrivateRoute path="/administrator">
-//                     <AdminView />
-//                 </PrivateRoute>
-//             </Switch>
-//         </Router>
-//     );
-// }
-
-// const mapStateToProps = (state) => {
-//     return {
-//         isAuthenticated: state.auth.isAuthenticated,
-//     };
-// };
-
-// export default connect(mapStateToProps, null)(App);
-
-import React, { Component } from "react";
-import "./App.css";
-import Amplify, { Auth, Interactions } from "aws-amplify";
-import { withAuthenticator, ChatBot, AmplifyTheme } from "aws-amplify-react";
-import aws_exports from "./aws-exports"; // specify the location of aws-exports.js file on your project
-Amplify.configure(aws_exports);
-const myTheme = {
-    ...AmplifyTheme,
-    sectionHeader: {
-        ...AmplifyTheme.sectionHeader,
-        backgroundColor: "#222",
-    },
-};
-class App extends Component {
-    handleComplete(err, confirmation) {
-        if (err) {
-            alert("Bot conversation failed");
-            return;
-        }
-        alert("Success: " + JSON.stringify(confirmation, null, 2));
-        return "Appointment booked. Thank you! What would you like to do next?";
-    }
-
-    render() {
+function App({ isAuthenticated }) {
+    /**
+     * Only grant access to these pages if user is logged in as administrator or staff
+     * Otherwise, redirect to login.
+     */
+    const PrivateRoute = ({ children, ...rest }) => {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <h1 className="App-title">Welcome to ReactBot</h1>
-                </header>
-                <p className="App-intro">
-                    <ChatBot
-                        title="My React Bot"
-                        theme={myTheme}
-                        botName="ScheduleAppointment_dev"
-                        welcomeMessage="Welcome, how can I help you today?"
-                        onComplete={this.handleComplete.bind(this)}
-                        clearOnComplete={true}
-                    />
-                </p>
-            </div>
+            <Route
+                {...rest}
+                render={({ location }) =>
+                    isAuthenticated ? (
+                        children
+                    ) : (
+                        <Redirect
+                            to={{
+                                pathname: "/login",
+                                state: { from: location },
+                            }}
+                        />
+                    )
+                }
+            />
         );
-    }
+    };
+
+    return (
+        <Router>
+            <TopNavbar />
+
+            <Switch>
+                <Route exact path="/">
+                    <LandingPage />
+                </Route>
+                <Route path="/login">
+                    <LoginPage />
+                </Route>
+                <Route path="/announcements">
+                    <AnnouncementPage />
+                </Route>
+                <PrivateRoute path="/administrator">
+                    <AdminView />
+                </PrivateRoute>
+            </Switch>
+        </Router>
+    );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+    return {
+        isAuthenticated: state.auth.isAuthenticated,
+    };
+};
+
+export default connect(mapStateToProps, null)(App);
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -3,105 +3,135 @@ import { Formik, Form, useField } from "formik";
 import styled from "@emotion/styled";
 import "./FormStyles.css";
 import * as Yup from "yup";
+import { ChatBot, AmplifyTheme } from "aws-amplify-react";
+
+const myTheme = {
+    ...AmplifyTheme,
+    sectionHeader: {
+        ...AmplifyTheme.sectionHeader,
+        backgroundColor: "#ff6600",
+    },
+};
 
 const LandingPage = () => {
-  const onSubmit = (formParameters) => {
-    // Check form parameters and do necessary
-    console.log("submitting");
-  };
-  return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-      }}
-    >
-      <div>
-        <h2>Visitor Registration</h2>
-        <>
-          <Formik
-            initialValues={{
-              name: "",
-              nric: "",
-              contact: "",
-              patientName: "",
-              patientNric: "",
-            }}
-            validationSchema={Yup.object({
-              name: Yup.string().required("Required"),
-              nric: Yup.string().required("Required"),
-              contact: Yup.string().required("Required"),
-              patientName: Yup.string().required("Required"),
-              patientNric: Yup.string().required("Required"),
-            })}
-            onSubmit={(values, { setSubmitting }) => {
-              setTimeout(() => {
-                onSubmit(JSON.stringify(values, null, 2));
-                setSubmitting(false);
-              }, 400);
-            }}
-          >
-            <Form
-              style={{
-                alignItems: "flex-start",
-                flexDirection: "column",
+    const handleComplete = (err, confirmation) => {
+        if (err) {
+            alert("Bot conversation failed");
+            return;
+        }
+        alert("Success: " + JSON.stringify(confirmation, null, 2));
+        return "Appointment booked. Thank you! What would you like to do next?";
+    };
+    const onSubmit = (formParameters) => {
+        // Check form parameters and do necessary
+        console.log("submitting");
+    };
+    return (
+        <div
+            style={{
                 display: "flex",
-              }}
-            >
-              <MyTextInput
-                label="Your Name"
-                name="name"
-                type="text"
-                placeholder="John Smith"
-              />
-              <MyTextInput
-                label="Your NRIC/Passport Number"
-                name="nric"
-                type="text"
-                placeholder="S1234567A"
-              />
-              <MyTextInput
-                label="Your Contact Number"
-                name="contact"
-                type="number"
-                placeholder="91234567"
-              />
-              <MyTextInput
-                label="Patient Name"
-                name="patientName"
-                type="text"
-                placeholder="Mary Jane"
-              />
-              <MyTextInput
-                label="Patient NRIC/Passport Number"
-                name="patientNric"
-                type="text"
-                placeholder="S9876543Z"
-              />
+                flexDirection: "column",
+                alignItems: "center",
+            }}
+        >
+            <header className="App-header">
+                <h1 className="App-title">Welcome to ReactBot</h1>
+            </header>
+            <p className="App-intro">
+                <ChatBot
+                    title="My React Bot"
+                    theme={myTheme}
+                    botName="ScheduleAppointment_dev"
+                    welcomeMessage="Welcome, how can I help you today?"
+                    onComplete={handleComplete}
+                    clearOnComplete={true}
+                />
+            </p>
+            <div>
+                <h2>Visitor Registration</h2>
+                <>
+                    <Formik
+                        initialValues={{
+                            name: "",
+                            nric: "",
+                            contact: "",
+                            patientName: "",
+                            patientNric: "",
+                        }}
+                        validationSchema={Yup.object({
+                            name: Yup.string().required("Required"),
+                            nric: Yup.string().required("Required"),
+                            contact: Yup.string().required("Required"),
+                            patientName: Yup.string().required("Required"),
+                            patientNric: Yup.string().required("Required"),
+                        })}
+                        onSubmit={(values, { setSubmitting }) => {
+                            setTimeout(() => {
+                                onSubmit(JSON.stringify(values, null, 2));
+                                setSubmitting(false);
+                            }, 400);
+                        }}
+                    >
+                        <Form
+                            style={{
+                                alignItems: "flex-start",
+                                flexDirection: "column",
+                                display: "flex",
+                            }}
+                        >
+                            <MyTextInput
+                                label="Your Name"
+                                name="name"
+                                type="text"
+                                placeholder="John Smith"
+                            />
+                            <MyTextInput
+                                label="Your NRIC/Passport Number"
+                                name="nric"
+                                type="text"
+                                placeholder="S1234567A"
+                            />
+                            <MyTextInput
+                                label="Your Contact Number"
+                                name="contact"
+                                type="number"
+                                placeholder="91234567"
+                            />
+                            <MyTextInput
+                                label="Patient Name"
+                                name="patientName"
+                                type="text"
+                                placeholder="Mary Jane"
+                            />
+                            <MyTextInput
+                                label="Patient NRIC/Passport Number"
+                                name="patientNric"
+                                type="text"
+                                placeholder="S9876543Z"
+                            />
 
-              <button style={{ marginBottom: 20 }} type="submit">
-                Submit
-              </button>
-            </Form>
-          </Formik>
-        </>
-      </div>
-    </div>
-  );
+                            <button style={{ marginBottom: 20 }} type="submit">
+                                Submit
+                            </button>
+                        </Form>
+                    </Formik>
+                </>
+            </div>
+        </div>
+    );
 };
 
 const MyTextInput = ({ label, ...props }) => {
-  const [field, meta] = useField(props);
-  return (
-    <>
-      <label htmlFor={props.id || props.name}>{label}</label>
-      <input className="text-input" {...field} {...props} />
-      {meta.touched && meta.error ? (
-        <div className="error">{meta.error}</div>
-      ) : null}
-    </>
-  );
+    const [field, meta] = useField(props);
+    return (
+        <>
+            <label htmlFor={props.id || props.name}>{label}</label>
+            <input className="text-input" {...field} {...props} />
+            {meta.touched && meta.error ? (
+                <div className="error">{meta.error}</div>
+            ) : null}
+        </>
+    );
 };
 
 export default LandingPage;
