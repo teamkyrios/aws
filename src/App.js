49d882af78f249a1ac5bc4e6580c7c1bc39c007a
hansebastian <sebastian_hans@hotmail.com>
2020-08-24 23:46:48 +0800

    Link login form to backend

--- src/App.js
+++ src/App.js
@@ -6,18 +6,9 @@ import TopNavbar from './components/TopNavbar';
 import AnnouncementPage from './components/AnnouncementPage';
 import LoginPage from './components/LoginPage';
 import AdminView from './components/admin/AdminView';
-<<<<<<< HEAD
-import connection from './Database';
-=======
 import StaffView from './components/staff/StaffView';
->>>>>>> a8e27c8ae3b76ec71abda0ad398ae2ecb134d543
 
 function App({ isAuthenticated }) {
-	connection.query('SELECT 1', function (error, results, fields) {
-		if (error) throw error;
-		alert('connected'); // connected!
-	});
-
 	/**
 	 * Only grant access to these pages if user is logged in as administrator or staff
 	 * Otherwise, redirect to login.
@@ -59,7 +50,7 @@ function App({ isAuthenticated }) {
 					<AdminView />
 				</PrivateRoute>
 				<PrivateRoute path='/staff'>
-					<StaffView/>
+					<StaffView />
 				</PrivateRoute>
 			</Switch>
 		</Router>
@@ -72,4 +63,4 @@ const mapStateToProps = (state) => {
 	};
 };
 
-export default connect(mapStateToProps, null)(App); 
+export default connect(mapStateToProps, null)(App);
