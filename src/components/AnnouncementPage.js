890e91fbda85c10b7cd9a21dcd1c2ed24fbedf19
hansebastian <sebastian_hans@hotmail.com>
2020-08-28 00:31:38 +0800

    Refactor staff view files under staff folder

--- src/components/AnnouncementPage.js
+++ src/components/AnnouncementPage.js
@@ -5,62 +5,10 @@ import Alert from '@material-ui/lab/Alert';
 import AlertTitle from '@material-ui/lab/AlertTitle';
 import WardViewTable from './WardViewTable';
 
-/**
- * Alert banner which will show when there are visitors who want to visit, banner can be closed by staff.
- * @param {*} props
- */
-const TopBanner = (props) => {
-	if (!props.isShowing) return null;
-
-	return (
-		<Alert severity='info' onClose={props.closeBanner}>
-			<AlertTitle>
-				<strong>Pending Visitors </strong>
-			</AlertTitle>
-			<p>There are visitors awaiting entry for wards 3, 5 and 16 !</p>
-			There are {props.liveVisitorNo} visitors currently
-		</Alert>
-	);
-};
-
-Modal.setAppElement('#root');
-
 const AnnoucementPage = () => {
-	useEffect(() => {
-		getUpdatedVisitorCount();
-	}, []);
-
-	const [liveVisitorNo, setLiveVisitorNo] = useState(115);
-	const [isBannerShowing, setIsBannerShowing] = useState(true);
-	const [allWards, setAllWards] = useState([
-		createData('Hans', 4, 1, 12, 4),
-		createData('Rui Feng', 4, 2, 12, 1),
-		createData('Max', 4, 3, 12, 2),
-		createData('Jun Xue', 4, 4, 12, 3),
-		createData('Bayes', 1, 1, 10, 2),
-		createData('Xuan Yi', 2, 2, 10, 3),
-		createData('Tomas', 3, 3, 11, 1),
-		createData('Toppiex', 5, 2, 13, 6),
-	]);
-
-	// Subscribe to DB changes
-	const getUpdatedVisitorCount = () => {};
-
-	/**
-	 * Format data into an object
-	 */
-	function createData(name, wardNumber, bedNumber, floorNumber, currVisitors) {
-		return { name, wardNumber, bedNumber, floorNumber, currVisitors };
-	}
-
 	return (
 		<div>
-			<TopBanner
-				isShowing={isBannerShowing}
-				closeBanner={() => setIsBannerShowing(false)}
-				liveVisitorNo={liveVisitorNo}
-			/>
-			<WardViewTable rows={allWards} />
+			<h1>Junxue was here</h1>
 		</div>
 	);
 };
--- src/components/staff/StaffView.js
+++ src/components/staff/StaffView.js
@@ -1,37 +1,69 @@
 import React, { useEffect, useState } from 'react';
-import './StaffView.css';
+import { connect } from 'react-redux';
+import Modal from 'react-modal';
+import Alert from '@material-ui/lab/Alert';
+import AlertTitle from '@material-ui/lab/AlertTitle';
+import WardViewTable from './WardViewTable';
 
 /**
- * the staff register brings them to a screen which displays the live number of visitors alongside the corresponding bed number
-in that screen they can toggle to the 4 different wards as well
+ * Alert banner which will show when there are visitors who want to visit, banner can be closed by staff.
+ * @param {*} props
+ */
+const TopBanner = (props) => {
+	if (!props.isShowing) return null;
 
-then in the staff page, they are also able to manually check a visitor out/in
-and they can also toggle the patient's allow Visitor condition
+	return (
+		<Alert severity='info' onClose={props.closeBanner}>
+			<AlertTitle>
+				<strong>Pending Visitors </strong>
+			</AlertTitle>
+			<p>There are visitors awaiting entry for wards 3, 5 and 16 !</p>
+			There are {props.liveVisitorNo} visitors currently
+		</Alert>
+	);
+};
+
+Modal.setAppElement('#root');
+
+/**
+ * Staff views the visitor management system
  */
 const StaffView = () => {
 	useEffect(() => {
 		getUpdatedVisitorCount();
 	}, []);
 
-	const [liveVisitorNo, setLiveVisitorNo] = useState(0);
+	const [liveVisitorNo, setLiveVisitorNo] = useState(115);
+	const [isBannerShowing, setIsBannerShowing] = useState(true);
+	const [allWards, setAllWards] = useState([
+		createData('Hans', 4, 1, 12, 4),
+		createData('Rui Feng', 4, 2, 12, 1),
+		createData('Max', 4, 3, 12, 2),
+		createData('Jun Xue', 4, 4, 12, 3),
+		createData('Bayes', 1, 1, 10, 2),
+		createData('Xuan Yi', 2, 2, 10, 3),
+		createData('Tomas', 3, 3, 11, 1),
+		createData('Toppiex', 5, 2, 13, 6),
+	]);
 
 	// Subscribe to DB changes
 	const getUpdatedVisitorCount = () => {};
 
+	/**
+	 * Format data into an object
+	 */
+	function createData(name, wardNumber, bedNumber, floorNumber, currVisitors) {
+		return { name, wardNumber, bedNumber, floorNumber, currVisitors };
+	}
+
 	return (
 		<div>
-			<h1>Staff View</h1>
-			<div id='StaffViewVisitorCountBanner'>
-				<h3>Live visitor number: {liveVisitorNo}</h3>
-			</div>
-			<h3>Detailed view of visitiors:</h3>
-			<div>
-				<p>Visitor - - - - Patient bed number</p>
-				<p>[Button to view different 4 wards]</p>
-			</div>
-			<div>
-				<p>Allow visitor to check in</p>
-			</div>
+			<TopBanner
+				isShowing={isBannerShowing}
+				closeBanner={() => setIsBannerShowing(false)}
+				liveVisitorNo={liveVisitorNo}
+			/>
+			<WardViewTable rows={allWards} />
 		</div>
 	);
 };
