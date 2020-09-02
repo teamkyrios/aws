4cdd9101b6e68fe3fe8c04ec8d05cc50acdd0c76
LimJunxue <junxue1@hotmail.com>
2020-09-02 18:19:31 +0800

    shift admin contents into admin page

--- src/components/admin/AdminView.js
+++ src/components/admin/AdminView.js
@@ -1,14 +1,147 @@
-import React from "react";
+import React, { Component } from "react";
+import { Formik, Form, useField } from "formik";
+import * as Yup from "yup";
+import { Button, ButtonGroup } from "reactstrap";
+import VisitationRulesForm from "./VisitationRulesForm";
 
-/**
- * Page of website that is only accessed by administrator.
- */
-const AdminView = () => {
+const MyTextAreaInput = ({ label, ...props }) => {
+	const [field, meta] = useField(props);
 	return (
-		<div>
-			<h1>Administrator view</h1>
-		</div>
+		<>
+			<label htmlFor={props.id || props.name}>{label}</label>
+			<textarea {...field} {...props} />
+			{meta.touched && meta.error ? (
+				<div className="error">{meta.error}</div>
+			) : null}
+		</>
 	);
 };
+
+// Takes in a string and uploads onto announcement database
+const AnnouncementForm = () => {
+	return (
+		<>
+			<hr />
+			<Formik
+				initialValues={{
+					announcement: "",
+				}}
+				validationSchema={Yup.object({
+					announcement: Yup.string()
+						.min(1, "Must be at least 1 character long")
+						.required("Required"),
+				})}
+				onSubmit={(values, { resetForm }) => {
+					console.log(
+						"Loading announcement: " + values["announcement"]
+					);
+					// put in backend
+					fetch(
+						"http://kyrios-env.eba-kvpkgwmc.us-east-1.elasticbeanstalk.com/updateAnnouncement",
+						{
+							method: "POST",
+							headers: {
+								"Content-Type": "application/json",
+							},
+							body: JSON.stringify({
+								announcement: values["announcement"],
+							}),
+						}
+					)
+						.then((res) => res.text())
+						.then((res) => alert(res))
+						.catch((err) => alert("Error updating announcement."));
+					resetForm({ values: "" });
+				}}
+			>
+				<Form
+					style={{
+						alignItems: "flex-start",
+						flexDirection: "column",
+						display: "flex",
+					}}
+				>
+					<MyTextAreaInput
+						label="Make an announcement"
+						name="announcement"
+						component="textarea"
+						rows="3"
+						placeholder="Type announcement here"
+					/>
+					<button style={{ borderRadius: 10 }} type="submit">
+						Announce
+					</button>
+				</Form>
+			</Formik>
+		</>
+	);
+};
+
+class AdminView extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			adminTab: 2,
+		};
+	}
+
+	changeAdminTab(index) {
+		this.setState({
+			adminTab: index,
+		});
+	}
+
+	render() {
+		let currAdminTab;
+		if (this.state.adminTab === 0) {
+			currAdminTab = <AnnouncementForm />;
+		} else if (this.state.adminTab === 1) {
+			currAdminTab = <VisitationRulesForm />;
+		}
+
+		return (
+			<div>
+				<div
+					style={{
+						display: "flex",
+						flexDirection: "column",
+						alignItems: "center",
+						margin: "10px",
+					}}
+				>
+					<h2>Administrator</h2>
+					<div
+						style={{
+							flexDirection: "row",
+						}}
+					>
+						<ButtonGroup>
+							<Button
+								size="lg"
+								onClick={() => this.changeAdminTab(0)}
+							>
+								<strong>Make an announcement</strong>
+							</Button>
+							<Button
+								size="lg"
+								onClick={() => this.changeAdminTab(1)}
+							>
+								<strong>Change visitation rules</strong>
+							</Button>
+						</ButtonGroup>
+					</div>
+					<div
+						style={{
+							flexDirection: "column",
+						}}
+					>
+						{currAdminTab}
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
 
 export default AdminView;
--- src/components/admin/ToggleVisitors.js
+++ src/components/admin/ToggleVisitors.js
@@ -4,110 +4,110 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
 
 const customStyles = {
-  content: {
-    top: "50%",
-    left: "50%",
-    right: "auto",
-    bottom: "auto",
-    marginRight: "-50%",
-    transform: "translate(-50%, -50%)",
-  },
+	content: {
+		top: "50%",
+		left: "50%",
+		right: "auto",
+		bottom: "auto",
+		marginRight: "-50%",
+		transform: "translate(-50%, -50%)",
+	},
 };
 
 // UI for modal component, including form to toggle visitors allowed status
 const ModalDetails = ({ name, nric, visitorsAllowedStatus, closeModal }) => {
-  const CheckStatus = ({ visitorsAllowedStatus }) => {
-    if (String(visitorsAllowedStatus).includes("does not allow visitors")) {
-      return false;
-    } else {
-      return true;
-    }
-  };
+	// const CheckStatus = () => {
+	// 	if (
+	// 		String({ visitorsAllowedStatus }).includes(
+	// 			"does not allow visitors"
+	// 		)
+	// 	) {
+	// 		return "Allow visitors for this patient";
+	// 	} else {
+	// 		return "Don't allow visitors for this patient";
+	// 	}
+	// };
 
-  const ChangeStatus = () => {
-    // toggle visitor
-    console.log(name);
-    console.log(nric);
-    fetch(
-      "http://kyrios-env.eba-kvpkgwmc.us-east-1.elasticbeanstalk.com/toggleVisitor",
-      {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          patientName: name,
-          patientNric: nric,
-        }),
-      }
-    )
-      .then((res) => res.json())
-      .then((res) => console.log(res.message))
-      .catch((err) => console.log(err));
-    //closeModal();
-  };
+	const ChangeStatus = () => {
+		// toggle visitor
+		fetch(
+			"http://kyrios-env.eba-kvpkgwmc.us-east-1.elasticbeanstalk.com/toggleVisitor",
+			{
+				method: "PUT",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					patientName: name,
+					patientNric: nric,
+				}),
+			}
+		)
+			.then((res) => res.json())
+			.then((res) => alert(res.message))
+			.catch((err) => console.log(err));
+		closeModal();
+	};
 
-  return (
-    <div style={{ display: "flex", flexDirection: "column" }}>
-      <FontAwesomeIcon
-        icon={faTimes}
-        onClick={closeModal}
-        style={{ position: "relative", left: "90%", top: "10%" }}
-      />
-      <p>Patient's name: {name}</p>
-      <p>Patient's NRIC: {nric}</p>
-      <p>
-        <strong>{visitorsAllowedStatus}.</strong>
-      </p>
+	return (
+		<div style={{ display: "flex", flexDirection: "column" }}>
+			<FontAwesomeIcon
+				icon={faTimes}
+				onClick={closeModal}
+				style={{ position: "relative", left: "90%", top: "10%" }}
+			/>
+			<p>Patient's name: {name}</p>
+			<p>Patient's NRIC: {nric}</p>
+			<p>
+				<strong>{visitorsAllowedStatus}.</strong>
+			</p>
 
-      <form
-        onSubmit={(event) => {
-          ChangeStatus();
-          event.preventDefault();
-        }}
-      >
-        <button
-          style={{
-            borderRadius: "10px",
-          }}
-          type="submit"
-        >
-          {CheckStatus(visitorsAllowedStatus)
-            ? "Don't allow visitors for this patient"
-            : "Allow visitors for this patient"}
-        </button>
-      </form>
-    </div>
-  );
+			<form
+				onSubmit={(event) => {
+					ChangeStatus();
+					event.preventDefault();
+				}}
+			>
+				<button
+					style={{
+						borderRadius: "10px",
+					}}
+					type="submit"
+				>
+					Allow/block visitors for this patient
+				</button>
+			</form>
+		</div>
+	);
 };
 
 /**
  * Modal component for allowing/barring visitors from a specific patient, searched via admin form.
  */
 class ToggleVisitors extends Component {
-  constructor(props) {
-    super(props);
-  }
+	constructor(props) {
+		super(props);
+	}
 
-  render() {
-    return (
-      <div>
-        <Modal
-          isOpen={this.props.modalOpen}
-          onRequestClose={this.props.closeModal}
-          style={customStyles}
-          contentLabel="Example Modal"
-        >
-          <ModalDetails
-            name={this.props.patientName}
-            nric={this.props.patientNric}
-            visitorsAllowedStatus={this.props.visitorsAllowedStatus}
-            closeModal={this.props.closeModal}
-          />
-        </Modal>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<Modal
+					isOpen={this.props.modalOpen}
+					onRequestClose={this.props.closeModal}
+					style={customStyles}
+					contentLabel="Example Modal"
+				>
+					<ModalDetails
+						name={this.props.patientName}
+						nric={this.props.patientNric}
+						visitorsAllowedStatus={this.props.visitorsAllowedStatus}
+						closeModal={this.props.closeModal}
+					/>
+				</Modal>
+			</div>
+		);
+	}
 }
 
 export default ToggleVisitors;
